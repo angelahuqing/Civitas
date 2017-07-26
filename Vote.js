8df3e12232e53a0350b98caeb05f90de90ceb884
krtams <kellysfg2010@gmail.com>
2017-07-26 10:54:40 -0700

    Update Vote.js

--- Vote.js
+++ Vote.js
@@ -3,24 +3,24 @@ function checkState() {
   word = word.toLowerCase();
 
     if (word == "new jersey"){
-       document.getElementById("results").innerHTML = window.location.href = "NewJerseyVote.html";
+       document.getElementById("results").innerHTML = window.location.href = "newjerseyVote.html";
       }else if (word == "new york"){
-        document.getElementById("results").innerHTML = window.location.href = "NewYorkVote.html";
+        document.getElementById("results").innerHTML = window.location.href = "newyorkVote.html";
       }else if (word == "california"){
-        document.getElementById("results").innerHTML = window.location.href = "CaliVote.html";
+        document.getElementById("results").innerHTML = window.location.href = "caliVote.html";
       }else if (word == "ohio"){
-        document.getElementById("results").innerHTML = window.location.href = "OhioVote.html";
+        document.getElementById("results").innerHTML = window.location.href = "ohioVote.html";
       }else if (word == "texas"){
-        document.getElementById("results").innerHTML = window.location.href = "TexasVote.html";
+        document.getElementById("results").innerHTML = window.location.href = "texasVote.html";
       }else if (word == "florida"){
-        document.getElementById("results").innerHTML = window.location.href = "FloridaVote.html";
+        document.getElementById("results").innerHTML = window.location.href = "floridaVote.html";
       }else if (word == "illinois"){
-        document.getElementById("results").innerHTML = window.location.href = "IllinoisVote.html";
+        document.getElementById("results").innerHTML = window.location.href = "illinoisVote.html";
       }else if (word == "virginia"){
-        document.getElementById("results").innerHTML = window.location.href = "VirginiaVote.html";
+        document.getElementById("results").innerHTML = window.location.href = "virginiaVote.html";
       }else if (word == "pennsylvania"){
-        document.getElementById("results").innerHTML = window.location.href = "PennVote.html";
+        document.getElementById("results").innerHTML = window.location.href = "pennVote.html";
       }else if (word == "massachusetts"){
-        document.getElementById("results").innerHTML = window.location.href = "MassVote.html";
+        document.getElementById("results").innerHTML = window.location.href = "massVote.html";
       }
 }
